592407f5738840d59d3147f58344f3fb65e8d4cc
RdArcher <giuseppe226.nocerino@gmail.com>
2026-09-02 20:10:45 +0200

    Aggiunta dell'interfaccia DAO di Ordine

--- src/main/java/dao/OrdineDAO.java
+++ src/main/java/dao/OrdineDAO.java
@@ -1,5 +1,19 @@
 package dao;
 
+import java.sql.SQLException;
+import java.util.Collection;
+
+import model.OrdineBean;
+
 public interface OrdineDAO {
 		
+	public void salvaOrdine(OrdineBean ordine) throws SQLException;
+	
+	public void eliminaOrdine(int id_ordine) throws SQLException;
+	
+	public OrdineBean cercaOrdine(int id_ordine) throws SQLException;
+	
+	public Collection<OrdineBean> ritornaOrdini(String order) throws SQLException;
+	
+	public boolean aggiornaStato(int id_ordine, boolean stato) throws SQLException;
 }
