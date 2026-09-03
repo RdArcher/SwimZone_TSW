6d37e8e3ca7aef506147eae41e10de51beed2c3e
RdArcher <giuseppe226.nocerino@gmail.com>
2026-09-03 18:30:55 +0200

    Implementazione Servlet per l'accesso

---
+++ src/main/java/control/LoginServlet.java
@@ -0,0 +1,57 @@
+package control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import dao.UtenteDAO;
+import dao.UtenteDAOImpl;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.UtenteBean;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doPost(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		if(email==null || password==null) {
+			request.setAttribute("errore", "Compila i campi per accedere");
+			RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+			dis.forward(request, response);
+			return;
+		}
+		
+		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
+		UtenteDAO utenteDAO = new UtenteDAOImpl(ds);
+		
+		try {
+			UtenteBean utente = utenteDAO.doRetrieveByEmailAndPassword(email, password);
+			
+			if(utente!=null) {
+				HttpSession session = request.getSession();
+				session.setAttribute("utente", utente);
+			} else {
+				request.setAttribute("errore", "email o password non corretti");
+				RequestDispatcher dis = request.getRequestDispatcher("login.php");
+				dis.forward(request, response);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+            request.setAttribute("errore", "Errore interno del server. Riprova più tardi.");
+            RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+            dis.forward(request, response);
+		}
+	}
+}
