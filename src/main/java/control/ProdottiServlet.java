9aefdb2b9c619a3115c3d64a155adc046b11cd51
RdArcher <giuseppe226.nocerino@gmail.com>
2026-09-08 18:00:08 +0200

    Fix connessione DataSource

--- src/main/java/control/ProdottiServlet.java
+++ src/main/java/control/ProdottiServlet.java
@@ -10,26 +10,33 @@ import dao.ProdottoDAOImpl;
 import dao.ProdottoDAO;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ProdottoBean;
 
-public class ProdottiServlet extends HttpServlet{
+@WebServlet("/Prodotti")
+public class ProdottiServlet extends HttpServlet {
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		
-		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
-		ProdottoDAO prodottoDAO = new ProdottoDAOImpl(ds);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String id = request.getParameter("id");
 		String order = request.getParameter("order");
 		
 		try {
-			if(id!=null) {
+			// --- INIZIO CONNESSIONE JNDI ---
+			javax.naming.Context initCtx = new javax.naming.InitialContext();
+			javax.naming.Context envCtx = (javax.naming.Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/SwimZoneDB");
+			
+			ProdottoDAO prodottoDAO = new ProdottoDAOImpl(ds);
+			// --- FINE CONNESSIONE JNDI ---
+
+			if(id != null) {
 				int idProdotto = Integer.parseInt(id);
 				ProdottoBean prodotto = prodottoDAO.cercaProdotto(idProdotto);
-				
+
 				if(prodotto != null) {
 					request.setAttribute("prodotto", prodotto);
 					RequestDispatcher req = request.getRequestDispatcher("/WEB-INF/view/dettaglio_prodotto.jsp");
@@ -40,19 +47,20 @@ public class ProdottiServlet extends HttpServlet{
 			} else {
 				Collection<ProdottoBean> prodotti = prodottoDAO.doRetrieveAll(order);
 				request.setAttribute("prodotti", prodotti);
-                RequestDispatcher dis = request.getRequestDispatcher("catalogo.jsp");
+				
+                RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/view/catalogo.jsp");
                 dis.forward(request, response);
 			}
+			
 		} catch(NumberFormatException e) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID prodotto non valido");
-        } catch (SQLException e) {
+        } catch (SQLException | javax.naming.NamingException e) {
             e.printStackTrace();
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Errore di accesso al database");
         }
-		
 	}
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);		
 	}
 }
